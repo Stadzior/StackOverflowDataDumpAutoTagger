85a95fb7a616f04d5bd555a09da5927aa49cbda7
Stadzior <Stadzior@gmail.com>
2018-06-23 23:32:12 +0200

    First working version of autotagger.py.

--- autotagger.py
+++ autotagger.py
@@ -1,25 +1,32 @@
 import re
 import operator
+import xml.etree.ElementTree as ET
 from os import listdir
 from os.path import isfile, join
 from textblob.classifiers import NaiveBayesClassifier
 
-trainPart = 0.05  
+trainPart = 0.005  
 testPartCount = 5
 tagsMatchCount = 3
 
 if (trainPart < 0 or trainPart > 100):
     exit
-posts = open("Posts.xml", "r").read().decode()    
+posts = ET.fromstring(open("Posts.xml", "r", encoding="utf8").read()) 
 
 #Building training set
 trainSetCount = int(len(posts)*trainPart)
 print("Analyzing: " + str(trainSetCount) + " posts.")
 trainPosts = posts[:trainSetCount]
-trainSet = [(re.sub(re.compile('<.*?>'), '', post.get("Body")).replace("\n", ""), tag) for tag in post.get("Tags").replace("<", "").split(">")[0:len(tags)-1] for post in list(filter(lambda post: post.get("Tags") is not None, trainPosts))]
+trainSet = [(re.sub(re.compile('<.*?>'), '', post.get("Body")).replace("\n", ""), post.get("Tags").replace("<", "").split(">")[0:len(post.get("Tags"))-1]) for post in list(filter(lambda post: post.get("Tags") is not None, trainPosts))]
+
+classifierInput = []
+for post in trainSet:
+    body = post[0]
+    for tag in post[1]:
+        classifierInput.append((body, tag))
 
 #trains classifier
-classifier = NaiveBayesClassifier(trainSet)
+classifier = NaiveBayesClassifier(classifierInput)
 
 #Build test set
 testSet = posts[trainSetCount:trainSetCount+testPartCount]
